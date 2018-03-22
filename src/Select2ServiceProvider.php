013784090e72cb329dbc040c3aa25fe131e5964b
baijunyao <baijunyao@baijunyao.com>
2018-03-22 12:11:22 +0800

    :tada:初始版本完成

---
+++ src/Select2ServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Baijunyao\LaravelSelect2;
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Support\ServiceProvider;
+use Baijunyao\LaravelSelect2\Middleware\LaravelSelect2;
+
+class Select2ServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/resources/statics' => public_path('statics'),
+        ], 'public');
+        $kernel = $this->app[Kernel::class];
+        $kernel->pushMiddleware(LaravelSelect2::class);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
