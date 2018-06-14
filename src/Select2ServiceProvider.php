3384307f140220fa25eae876ad2fa36eb5553589
baijunyao <baijunyao@baijunyao.com>
2018-06-14 14:35:03 +0800

    增加select2的blade标签

--- src/Select2ServiceProvider.php
+++ src/Select2ServiceProvider.php
@@ -5,6 +5,8 @@ namespace Baijunyao\LaravelSelect2;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\ServiceProvider;
 use Baijunyao\LaravelSelect2\Middleware\LaravelSelect2;
+use Illuminate\Support\Facades\Blade;
+use Baijunyao\LaravelJquery\Jquery;
 
 class Select2ServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,26 @@ class Select2ServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/resources/statics' => public_path('statics'),
         ], 'public');
+
+        // 定义 select2 css 标签
+        Blade::directive('select2css', function () {
+            $select2CssPath = asset('statics/select2-4.0.4/css/select2.min.css');
+            $select2Css = <<<php
+<link href="$select2CssPath" rel="stylesheet" type="text/css" />
+php;
+            return $select2Css;
+        });
+
+        // 定义 select2 js 标签
+        Blade::directive('select2js', function () {
+            $select2JsPath = asset('statics/select2-4.0.4/js/select2.min.js');
+            $jquery = Jquery::unique();
+            $select2Js = <<<php
+$jquery
+<script src="$select2JsPath"></script>
+php;
+            return $select2Js;
+        });
     }
 
     /**
